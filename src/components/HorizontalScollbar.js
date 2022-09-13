2a93386cc3948d3818990fd16e077159ccc42afe
Sameer Bagwan <sameerbagwan132@gmail.com>
2022-09-13 16:10:58 +0530

    Final

--- src/components/HorizontalScollbar.js
+++ src/components/HorizontalScollbar.js
@@ -1,32 +1,32 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Box, Typography } from "@mui/material";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-import BodyPart from './BodyPart'
+import BodyPart from "./BodyPart";
 import LeftArrowIcon from "../assets/icons/left-arrow.png";
 import RightArrowIcon from "../assets/icons/right-arrow.png";
-import '../App.css';
+import "../App.css";
+import ExerciseCard from "./ExerciseCard";
 
 const LeftArrow = () => {
-    const { scrollPrev } = useContext(VisibilityContext);
-  
-    return (
-      <Typography onClick={() => scrollPrev()} className="right-arrow">
-        <img src={LeftArrowIcon} alt="right-arrow" />
-      </Typography>
-    );
-  };
-  
-  const RightArrow = () => {
-    const { scrollNext } = useContext(VisibilityContext);
-  
-    return (
-      <Typography onClick={() => scrollNext()} className="left-arrow">
-        <img src={RightArrowIcon} alt="right-arrow" />
-      </Typography>
-    );
-  };
+  const { scrollPrev } = useContext(VisibilityContext);
 
-const HorizontalScollbar = ({ data, bodyPart, setBodyPart }) => {
+  return (
+    <Typography mt='5px' mb='5px' onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+  return (
+    <Typography mt='5px' mb='5px' onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const HorizontalScollbar = ({ data, bodyPart, setBodyPart, isBodyPart }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
@@ -36,10 +36,17 @@ const HorizontalScollbar = ({ data, bodyPart, setBodyPart }) => {
           title={item.id || item}
           m="0 40px"
         >
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+          {isBodyPart ? (
+            <BodyPart
+              item={item}
+              bodyPart={bodyPart}
+              setBodyPart={setBodyPart}
+            />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
         </Box>
-      )
-      )}
+      ))}
     </ScrollMenu>
   );
 };
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,17 @@
 export const exerciseOptions = {
   method: "GET",
-  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
   headers: {
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
       "X-RapidAPI-Key": "11b4d6f032msh386aca822c0b025p110251jsnaf9a980bd21a"
   },
+};
+
+export const youtubeOption = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '11b4d6f032msh386aca822c0b025p110251jsnaf9a980bd21a'
+  }
 };
 
 export const fetchData = async (url, options) => {
