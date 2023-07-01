2154e04f03fc77178976f1f24bfa9038cae76497
Sameer <sameerbagwan132@gmail.com>
2023-07-01 21:29:46 +0530

    deploy

--- src/components/SimilairExercises.js
+++ src/components/SimilairExercises.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import HorizontalScollbar from "./HorizontalScollbar";
-import Loader from "./Loader";
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import HorizontalScollbar from './HorizontalScollbar';
+import Loader from './Loader';
 
 const SimilairExercises = ({ targetMuscleVideo, equipmentExericses }) => {
   return (
-    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3" mb={5}>
+    <Box sx={{ mt: { lg: '100px', xs: '0' } }}>
+      <Typography variant='h3' mb={5}>
         Exercises That Target The Same Muscle Group
       </Typography>
-      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+      <Stack direction='row' sx={{ p: '2', position: 'relative' }}>
         {targetMuscleVideo.length ? (
           <HorizontalScollbar data={targetMuscleVideo} />
         ) : (
@@ -17,10 +17,10 @@ const SimilairExercises = ({ targetMuscleVideo, equipmentExericses }) => {
         )}
       </Stack>
 
-      <Typography variant="h3" mb={5}>
+      <Typography variant='h3' mb={5}>
         Exercises That Use The Same Equipment Group
       </Typography>
-      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+      <Stack direction='row' sx={{ p: '2', position: 'relative' }}>
         {targetMuscleVideo.length ? (
           <HorizontalScollbar data={equipmentExericses} />
         ) : (
